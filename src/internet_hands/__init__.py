5a0d1aeb960d4f1a754c0c4b81a55c01d11036cf
Ike shanto <sphangcho203@gmail.com>
2026-09-13 07:01:43 +0530

    chore: bump Internet Hands package version to 0.5.0

--- src/internet_hands/__init__.py
+++ src/internet_hands/__init__.py
@@ -1,3 +1,3 @@
-"""Internet Hands: public-web collection, indexing, monitoring, and analysis primitives."""
+"""Internet Hands: public-web collection, indexing, monitoring, and agent tool primitives."""
 
-__version__ = "0.3.0"
+__version__ = "0.5.0"
