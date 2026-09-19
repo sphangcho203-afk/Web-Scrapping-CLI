bd56a055c68dfe5ed96f4a2c9aba2dfe01824828
Ike shanto <sphangcho203@gmail.com>
2026-09-19 09:28:10 +0530

    feat(monitors): wire scheduler execution route

--- src/internet_hands/saas_app.py
+++ src/internet_hands/saas_app.py
@@ -11,6 +11,7 @@ from .control_hardening import router as hardening_router
 from .fleet_api import app as fleet_app
 from .mcp_customer import customer_streamable_http_app
 from .mcp_server import sandbox_mcp
+from .monitor_executor import router as monitor_executor_router
 from .monitor_lifecycle import router as monitor_lifecycle_router
 from .oauth_compat import router as oauth_compat_router
 from .security_api import router as security_router
@@ -42,6 +43,7 @@ app.include_router(oauth_compat_router)
 app.include_router(security_hardening_router)
 app.include_router(security_router)
 app.include_router(hardening_router)
+app.include_router(monitor_executor_router)
 app.include_router(monitor_lifecycle_router)
 app.include_router(control_router)
 app.include_router(usage_router)
